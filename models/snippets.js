be4f8f3e7329c7c465e5007a66a7a0733807d9ef
Sonick Mumba <kingellie.mumba@gmail.com>
2025-07-18 01:58:26 +0200

    Update snippet.js

--- models/snippets.js
+++ models/snippets.js
@@ -1,6 +1,6 @@
 const pool = require('./db');
 
-const snippert = {
+const Snippet = {
   sendSnippet: async (id, userId, code, language, theme) => {
     const query = `INSERT INTO snippets (id, user_id, code, language, theme) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
     const { rows } = await pool.query(query, [id, userId, code, language, theme]);
@@ -14,3 +14,5 @@ const snippert = {
     return rows;
   }
 }
+
+module.exports = Snippet
