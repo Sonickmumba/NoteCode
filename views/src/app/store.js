93a21f8b6e2f8a302d0ad8ef3bd9f732fe528f0a
Sonick Mumba <kingellie.mumba@gmail.com>
2025-07-18 09:21:56 +0200

    Implement react store

---
+++ views/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from '../features/auth/authSlice';
+// import userReducer from '../features/users/userSlice';
+import snippetReducer from '../features/snippets/snippetSlice';
+
+export const store = configureStore({
+  reducer: {
+    // auth: authReducer,
+    // users: userReducer,
+    snippets: snippetReducer,
+  },
+});
